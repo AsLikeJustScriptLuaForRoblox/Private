f58e4fe761c348225c83e7fc00847729ed6acb66
AsLikeJustScriptLuaForRoblox <hilosarry0@gmail.com>
2025-04-09 11:13:46 +0700

    Create 1 years for sex ugc.lua

---
+++ 1 years for sex ugc.lua
@@ -0,0 +1,20 @@
+if game.PlaceId == 90998286454316 then
+spawn(function()
+        for _, descendant in ipairs(game:GetService("Workspace").Easy_Obbys.ObbyPortals.Unbeatable.WinPoint:GetDescendants()) do
+            if descendant:IsA("TouchTransmitter") and
+                    game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character.PrimaryPart then
+                task.spawn(function()
+                    firetouchinterest(descendant:FindFirstAncestorWhichIsA("Part"),
+                        game.Players.LocalPlayer.Character.PrimaryPart, 1)
+                    task.wait()
+                    firetouchinterest(descendant:FindFirstAncestorWhichIsA("Part"),
+                        game.Players.LocalPlayer.Character.PrimaryPart, 0)
+                end)
+            end
+        end
+    end)
+    game.Players.LocalPlayer.Character:PivotTo(CFrame.new(game.Workspace.Easy_Obbys.ObbyPortals.Unbeatable.WinPoint.Position))
+    wait(2) 
+    game.Players.LocalPlayer:Kick("GET OUT") 
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+end
